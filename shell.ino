0b6b89b7bb1d3398015f0bcb845659bde72641f5
Stephen Wiley <swiley@Stephen-Wileys-MacBook-Pro-2.local>
2014-10-17 15:54:46 +0000

    should work untested added w

--- shell.ino
+++ shell.ino
@@ -1,40 +1,51 @@
-//ROBOTS ARE BETTER THEN GIRLFREINDS
-#define LED_N_SIDE 2
-#define LED_P_SIDE 3
-#define LAMP 13
-#define LSF 9
-#define LSR 115
-
-#define RSF 114
-#define RSR 9
-
-#define LSP 10
-#define RSP 9
-#define BOUSTT_T 1000  //aproxomet time for a 100 gradian tank turn
-#define BOUSTF_T 1000  //aproxomet time to travel width of robot mop
-#define servopower 11
-
-#include <Servo.h>
 const int ledPin = 13;
 extern char _umem ;
+char buf[162];
+unsigned int umeml=48000;
 
 // the setup() method runs once, when the sketch starts
 
 void setup() {
 	// initialize the digital pin as an output.
 	pinMode(ledPin, OUTPUT);
+	digitalWrite(ledPin, HIGH);
 	Serial.begin(9600);
+	Serial.print("user mem begins at ");
+	Serial.println((unsigned int)&_umem,HEX);
+
 }
 
 // the loop() methor runs over and over again,
 // as long as the board has power
+unsigned int nexti( char **c)
+{
+	char *b=*c;
+	for(;((**c!=0)&&(**c!=' '));*c++);
+	return (unsigned int)strtol(b,c,16);
+}
+void write(void)
+{
+	char *c=&buf[1];
+	unsigned int l=nexti(&c);
+	char *m=(char *)l;
+	if((l< (unsigned int)&_umem) || l>(unsigned int)&_umem+umeml)
+	{
+		Serial.println("no");
+		return;
+	}
+	while(*c!=0)
+	{
+		*m=nexti(&c);
+		m++;
+	}
+}
 
 void loop() {
-	digitalWrite(ledPin, HIGH);		// set the LED on
-	delay(1000);									// wait for a second
-	digitalWrite(ledPin, LOW);		// set the LED off
-	delay(1000);									// wait for a second
-	Serial.println("hello world");
-	Serial.println((unsigned int)&_umem,HEX);
+	Serial.print("@");
+	Serial.readBytesUntil('\n', buf, 162);
+	switch(buf[0]){
+		case 'w': write();break;
+		default: Serial.println("?");
+	}
 }
 
